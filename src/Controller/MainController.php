83cf01b3d3a8906a837f7d50fb20558900643fe2
yannick <yannick@yhonore.fr>
2022-11-27 16:31:09 +0100

    Intégration du module de traduction

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
@@ -14,5 +15,15 @@ class MainController extends AbstractController
         return $this->render('main/index.html.twig', [
             
         ]);
+    }
+
+    #[Route('/change-locale/{locale}', name: 'change_locale')]
+    public function changeLocale($locale, Request $request): Response
+    {
+        // On stocke la langue demandée dans la session
+        $request->getSession()->set('_locale', $locale);
+
+        // On revien sur la page précédente
+        return $this->redirect($request->headers->get('referer'));
     }
 }
